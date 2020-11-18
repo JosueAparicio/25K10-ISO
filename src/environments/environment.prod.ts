5bed0eb769b57abe10d11717926e11f919d71271
JosueAparicio <josue9aparicio@gmail.com>
2020-11-17 21:15:58 -0600

    version de prod lista: hosting funcionando.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBSON0JpPoDN_1iyuqldaaeqXdjOTePgrE",
+    authDomain: "iso-proscanner.firebaseapp.com",
+    databaseURL: "https://iso-proscanner.firebaseio.com",
+    projectId: "iso-proscanner",
+    storageBucket: "iso-proscanner.appspot.com",
+    messagingSenderId: "928732941253",
+    appId: "1:928732941253:web:eb474ab6b616d0972219ac"
+  }
 };
